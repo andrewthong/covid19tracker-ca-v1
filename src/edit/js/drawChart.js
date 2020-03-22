ed68b7c89726495ad78d63c6b6be2fca6e1215b5
agulbs <alekgulbenkian@gmail.com>
2020-03-22 16:45:11 -0400

    fixed bar graph so config gets passsed throguh

--- src/edit/js/drawChart.js
+++ src/edit/js/drawChart.js
@@ -13,7 +13,6 @@ function toShortFormat(dt) {
 }
 
 function lineGraph(data, id, flag) {
-
 	var name = [];
 	var marks = [];
 	var sum = 0;
@@ -33,37 +32,77 @@ function lineGraph(data, id, flag) {
 		}
 	}
 
-	var chartdata = {
-		labels: name,
-		datasets: [{
-			label: "Cases",
-			lineTension: 0.3,
-			backgroundColor: "rgba(2,117,216,0.2)",
-			borderColor: "rgba(2,117,216,1)",
-			pointRadius: 5,
-			pointBackgroundColor: "rgba(2,117,216,1)",
-			pointBorderColor: "rgba(255,255,255,0.8)",
-			pointHoverRadius: 5,
-			pointHoverBackgroundColor: "rgba(2,117,216,1)",
-			pointHitRadius: 50,
-			pointBorderWidth: 2,
-			data: marks
-		}]
-	};
+	var graphConfig = {
+		graphTarget: $(id),
+		type: 'line',
+		name: name,
+		marks: marks,
+		sum: sum,
+		unit: 'date',
+		chartdata: {
+			labels: name,
+			datasets: [{
+				lineTension: 0.3,
+				pointRadius: 5,
+				backgroundColor: "rgba(2,117,216,0.2)",
+				borderColor: "rgba(2,117,216,1)",
+				pointBackgroundColor: "rgba(2,117,216,1)",
+				pointBorderColor: "rgba(255,255,255,0.8)",
+				pointHoverRadius: 5,
+				pointHoverBackgroundColor: "rgba(2,117,216,1)",
+				pointHitRadius: 50,
+				pointBorderWidth: 2,
+				data: marks
+			}],
+		}
+	}
 
+	draw(graphConfig)
+
+}
+
+function barGraph(data, id) {
+
+	var name = [];
+	var marks = [];
+
+	for (var i in data) {
+		name.push(data[i].province);
+		marks.push(data[i].cases);
+	}
+
+
+	var graphConfig = {
+		graphTarget: $(id),
+		type: 'bar',
+		unit: 'month',
+		chartdata: {
+			labels: name,
+			datasets: [{
+				label: "Cases",
+				backgroundColor: "rgba(2,117,216,1)",
+				borderColor: "rgba(2,117,216,1)",
+				data: marks
+			}]
+		}
+	}
+
+	draw(graphConfig)
+
+}
+
+function draw(graphConfig) {
 	Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 	Chart.defaults.global.defaultFontColor = '#292b2c';
 
-	var graphTarget = $(id);
-
-	var barGraph = new Chart(graphTarget, {
-		type: 'line',
-		data: chartdata,
+	var chart = new Chart(graphConfig.graphTarget, {
+		type: graphConfig.type,
+		data: graphConfig.chartdata,
 		options: {
 			scales: {
 				xAxes: [{
 					time: {
-						unit: 'date'
+						unit: graphConfig.unit
 					},
 					gridLines: {
 						display: false
@@ -79,66 +118,6 @@ function lineGraph(data, id, flag) {
 					},
 					gridLines: {
 						color: "rgba(0, 0, 0, .125)",
-					}
-				}],
-			},
-			legend: {
-				display: false
-			}
-		}
-	});
-}
-
-function barGraph(data, id) {
-
-	var name = [];
-	var marks = [];
-
-	for (var i in data) {
-		name.push(data[i].province);
-		marks.push(data[i].cases);
-	}
-
-
-	var chartdata = {
-		labels: name,
-		/*labels : ["Ontario", "Quebec", "British Columbia", "Alberta", "Manitoba", "Saskatchewan", "Nova Scotia", "New Brunswick", "Newfoundland and Labrador", "Prince Edward Island", "Northwest Territories", "Nunavut", "Yukon", "Repatriated Canadians"],*/
-		datasets: [{
-			label: "Cases",
-			backgroundColor: "rgba(2,117,216,1)",
-			borderColor: "rgba(2,117,216,1)",
-			data: marks
-		}]
-	};
-
-	Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-	Chart.defaults.global.defaultFontColor = '#292b2c';
-
-	var graphTarget = $(id);
-
-	var barGraph = new Chart(graphTarget, {
-		type: 'bar',
-		data: chartdata,
-		options: {
-			scales: {
-				xAxes: [{
-					time: {
-						unit: 'month'
-					},
-					gridLines: {
-						display: false
-					},
-					ticks: {
-						maxTicksLimit: 14
-					}
-				}],
-				yAxes: [{
-					ticks: {
-						min: 0,
-						maxTicksLimit: 10
-					},
-					gridLines: {
-						display: true
 					}
 				}],
 			},
