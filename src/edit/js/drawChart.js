bb28b25f3bfd746b12e43a1b5c9b77819bfbe912
Alek Gulbenkian <agulbs@Aleks-MBP.home>
2020-03-25 14:37:04 -0400

    fixed death bug

--- src/edit/js/drawChart.js
+++ src/edit/js/drawChart.js
@@ -2,17 +2,17 @@
 // @param dt: str  a date
 // @return: str shortDateFormat
 function toShortFormat(dt) {
-	var month_names = ["Jan", "Feb", "Mar",
-		"Apr", "May", "Jun",
-		"Jul", "Aug", "Sep",
-		"Oct", "Nov", "Dec"
-	];
+    var month_names = ["Jan", "Feb", "Mar",
+        "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep",
+        "Oct", "Nov", "Dec"
+    ];
 
-	var day = dt.getDate();
-	var month_index = dt.getMonth();
-	var year = dt.getFullYear();
+    var day = dt.getDate();
+    var month_index = dt.getMonth();
+    var year = dt.getFullYear();
 
-	return "" + month_names[month_index] + " " + day;
+    return "" + month_names[month_index] + " " + day;
 }
 
 
@@ -21,51 +21,52 @@ function toShortFormat(dt) {
 // @param data: str id for the graph
 // @param data: boolean differntiate b/w summing vs appending
 function lineGraph(data, id, flag) {
-	var name = [];
-	var marks = [];
-	var sum = 0;
-	for (var i in data) {
-		var ddate = data[i].dte;
-		var cdate = toShortFormat(new Date(ddate));
-		name.push(cdate);
+    var name = [];
+    var marks = [];
+    var sum = 0;
+    for (var i in data) {
+        var ddate = data[i].dte;
+        var cdate = toShortFormat(new Date(ddate));
+        name.push(cdate);
 
-		if (flag) {
-			sum += parseInt(data[i].totalGCase);
-			marks.push(sum);
-		} else {
-			num = parseInt(data[i].totalGCase);
-			marks.push(num);
-		}
-	}
+        if (flag) {
+            sum += parseInt(data[i].totalGCase);
+            marks.push(sum);
+        } else {
+            num = parseInt(data[i].totalGCase);
+            marks.push(num);
+        }
+    }
 
-	// used to setup graph that needs to be drawn
-	var graphConfig = {
-		graphTarget: $(id),
-		type: 'line',
-		name: name,
-		marks: marks,
-		sum: sum,
-		unit: 'date',
-		chartdata: {
-			labels: name,
-			datasets: [{
-				lineTension: 0.3,
-				pointRadius: 5,
-				backgroundColor: "rgba(2,117,216,0.2)",
-				borderColor: "rgba(2,117,216,1)",
-				pointBackgroundColor: "rgba(2,117,216,1)",
-				pointBorderColor: "rgba(255,255,255,0.8)",
-				pointHoverRadius: 5,
-				pointHoverBackgroundColor: "rgba(2,117,216,1)",
-				pointHitRadius: 50,
-				pointBorderWidth: 2,
-				data: marks
-			}],
-		}
-	}
+    // used to setup graph that needs to be drawn
+    var graphConfig = {
+        graphTarget: $(id),
+        type: 'line',
+        name: name,
+        marks: marks,
+        sum: sum,
+        unit: 'date',
+        chartdata: {
+            labels: name,
+            datasets: [{
+                lineTension: 0.3,
+                pointRadius: 5,
+                backgroundColor: "rgba(2,117,216,0.2)",
+                borderColor: "rgba(2,117,216,1)",
+                pointBackgroundColor: "rgba(2,117,216,1)",
+                pointBorderColor: "rgba(255,255,255,0.8)",
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(2,117,216,1)",
+                pointHitRadius: 50,
+                pointBorderWidth: 2,
+                data: marks
+            }],
+        },
+        ticks: 7
+    }
 
-	// renders the graph
-	draw(graphConfig);
+    // renders the graph
+    draw(graphConfig);
 
 }
 
@@ -75,31 +76,32 @@ function lineGraph(data, id, flag) {
 // @param data: str id for the graph
 function barGraph(data, id) {
 
-	var name = [];
-	var marks = [];
+    var name = [];
+    var marks = [];
 
-	for (var i in data) {
-		name.push(data[i].province);
-		marks.push(data[i].cases);
-	}
+    for (var i in data) {
+        name.push(data[i].province);
+        marks.push(data[i].cases);
+    }
 
-	var graphConfig = {
-		graphTarget: $(id),
-		type: 'bar',
-		unit: 'month',
-		chartdata: {
-			labels: name,
-			datasets: [{
-				label: "Cases",
-				backgroundColor: "rgba(2,117,216,1)",
-				borderColor: "rgba(2,117,216,1)",
-				data: marks
-			}]
-		}
-	}
+    var graphConfig = {
+        graphTarget: $(id),
+        type: 'bar',
+        unit: 'month',
+        chartdata: {
+            labels: name,
+            datasets: [{
+                label: "Cases",
+                backgroundColor: "rgba(2,117,216,1)",
+                borderColor: "rgba(2,117,216,1)",
+                data: marks
+            }]
+        },
+        ticks: 15
+    }
 
-	// renders the graph
-	draw(graphConfig);
+    // renders the graph
+    draw(graphConfig);
 
 }
 
@@ -107,39 +109,39 @@ function barGraph(data, id) {
 // draw: renders the graph to HTML
 // @param graphConfig: {} config for graph
 function draw(graphConfig) {
-	Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-	Chart.defaults.global.defaultFontColor = '#292b2c';
+    Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+    Chart.defaults.global.defaultFontColor = '#292b2c';
 
-	var chart = new Chart(graphConfig.graphTarget, {
-		type: graphConfig.type,
-		data: graphConfig.chartdata,
-		options: {
-			scales: {
-				xAxes: [{
-					time: {
-						unit: graphConfig.unit
-					},
-					gridLines: {
-						display: false
-					},
-					ticks: {
-						maxTicksLimit: 7
-					}
-				}],
-				yAxes: [{
-					ticks: {
-						min: 0,
-						maxTicksLimit: 5
-					},
-					gridLines: {
-						color: "rgba(0, 0, 0, .125)",
-					}
-				}],
-			},
-			legend: {
-				display: false
-			}
-		}
-	});
+    var chart = new Chart(graphConfig.graphTarget, {
+        type: graphConfig.type,
+        data: graphConfig.chartdata,
+        options: {
+            scales: {
+                xAxes: [{
+                    time: {
+                        unit: graphConfig.unit
+                    },
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: {
+                        maxTicksLimit: graphConfig.ticks
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                        maxTicksLimit: 5
+                    },
+                    gridLines: {
+                        color: "rgba(0, 0, 0, .125)",
+                    }
+                }],
+            },
+            legend: {
+                display: false
+            }
+        }
+    });
 
 }
--- src/edit/js/state.js
+++ src/edit/js/state.js
@@ -1,129 +1,112 @@
 // Controls the state of the application, sets up correct data information
 $(document).ready(() => {
 
-	// on load make GET request to load all info required for index page
-	$.ajax({
-		url: url + "api/controller/cases.php",
-		// url: "http://localhost/Covid19TrackerCA/src/edit/api/controller/cases.php",
-		type: "GET",
-	}).then(res => {
+    // on load make GET request to load all info required for index page
+    $.ajax({
+        url: url + "api/controller/cases.php",
+        // url: "http://localhost/Covid19TrackerCA/src/edit/api/controller/cases.php",
+        type: "GET",
+    }).then(res => {
 
-		makeMap(res);
+        makeMap(res);
 
-		// override last update times + death & case count
-		$('#updateTime')[0].innerHTML = res["lastUpdate"];
-		$('#totalCases')[0].innerHTML = res["totalCases"]["cases"] + " ";
-		$('#totalCasesCanada')[0].innerHTML = res["totalCases"]["cases"] + " ";
-		$('#totalDeath')[0].innerHTML = res["totalCases"]["death"] + " ";
+        // override last update times + death & case count
+        $('#updateTime')[0].innerHTML = res["lastUpdate"];
+        $('#totalCases')[0].innerHTML = res["totalCases"]["cases"] + " ";
+        $('#totalCasesCanada')[0].innerHTML = res["totalCases"]["cases"] + " ";
+        $('#totalDeath')[0].innerHTML = res["totalCases"]["death"] + " ";
 
-		// get total death sum for canada
-		var death_sum = 0;
-		res['casesByProvince'].forEach(r => {
-			if (r.province in res['deathsByProvince'])
-				r['deaths'] = res['deathsByProvince'][r.province];
-			else
-				r['deaths'] = 0;
+        // get total death sum for canada
+        var death_sum = 0;
+        res['casesByProvince'].forEach(r => {
+            if (r.province in res['deathsByProvince'])
+                r['deaths'] = res['deathsByProvince'][r.province];
+            else
+                r['deaths'] = 0;
 
-			death_sum += (res['deathsByProvince'][r.province]);
-		});
+            death_sum += (res['deathsByProvince'][r.province]);
+        });
 
-		// total infected from canada
-		$('#infectedPerCanada')[0].innerHTML = Math.round((parseInt(res["totalCases"]["cases"]) / parseInt(res["casePerPopulation"]["Canada"]) * 100)) / 100;
-
-
-		// setup hash for map
+        // total infected from canada
+        $('#infectedPerCanada')[0].innerHTML = Math.round((parseInt(res["totalCases"]["cases"]) / parseInt(res["casePerPopulation"]["Canada"]) * 100)) / 100;
 
 
-		// calculate deaths & cases per province
-		var casesByProvince = {};
-		res["totalCaseProvince"].forEach(r => {
-			if (r.province in casesByProvince) {
-				//casesByProvince[r.province]["cases"] += parseInt(r.cases);
-				//casesByProvince[r.province]["deaths"] += parseInt(r.deaths);
-			} else {
-				casesByProvince[r.province] = r;
-				casesByProvince[r.province]["cases"] = parseInt(casesByProvince[r.province]["cases"]);
-				casesByProvince[r.province]["deaths"] = parseInt(casesByProvince[r.province]["deaths"]);
-			}
+        // calculate deaths & cases per province
+        var casesByProvince = {};
+        res["totalCaseProvince"].forEach(r => {
+            casesByProvince[r.province] = r;
+            casesByProvince[r.province]["cases"] = parseInt(casesByProvince[r.province]["cases"]);
+            casesByProvince[r.province]["deaths"] = parseInt(casesByProvince[r.province]["deaths"]);
+        })
 
-		})
+        console.log(casesByProvince);
+        // update total case by providence per 100,00
+        for (var province in casesByProvince) {
+            var calc = Math.round((parseInt(casesByProvince[province]["cases"]) / parseInt(res["casePerPopulation"][province]) * 100)) / 100;
+            if (isNaN(calc))
+                calc = 0;
 
-		var deaths_total = 0;
-		
-		console.log(casesByProvince);
-		// update total case by providence per 100,00
-		for (var province in casesByProvince) {
-			var calc = Math.round((parseInt(casesByProvince[province]["cases"]) / parseInt(res["casePerPopulation"][province]) * 100)) / 100;
-			if (isNaN(calc))
-				calc = 0;
+            // TODO: this one errors out, we need to fix
+            if (province == "Northwest Territories")
+                calc = 2.44;
 
-			// TODO: this one errors out, we need to fix
-			if (province == "Northwest Territories")
-				calc = 2.44;
+            // append data to row
+            $('#totalCasesProvinceTable').append(
+                "<tr>" +
+                "<td>" + province + "</td>" +
+                "<td><b><i>" + casesByProvince[province]["cases"] + "</i></b></td>" +
+                "<td>" + calc + "</td>" +
+                "<td>" + casesByProvince[province]["deaths"] + "</td>" +
+                "<td><a href='" + casesByProvince[province]["source"] + "'>Source</a></td>" +
+                "</tr>"
+            )
+        }
 
-			// append data to row
-			$('#totalCasesProvinceTable').append(
-				"<tr>" +
-				"<td>" + province + "</td>" +
-				"<td><b><i>" + casesByProvince[province]["cases"] + "</i></b></td>" +
-				"<td>" + calc + "</td>" +
-				"<td>" + casesByProvince[province]["deaths"] + "</td>" +
-				"<td><a href='" + casesByProvince[province]["source"] + "'>Source</a></td>" +
-				"</tr>"
-			)
+        $('.death_total')[0].innerHTML = res["totalCases"]["death"] + " ";
 
-			deaths_total += parseInt(casesByProvince[province]["deaths"]);
+        // draw 3 main graphs
+        lineGraph(res["cumulativeCases"], "#cumulativeCaseChart", true);
+        lineGraph(res["dailyCases"], "#dailyCaseChart", false);
+        barGraph(res["totalCaseProvince"], "#provinceCasesChart");
 
-			// update the maps info
+        // update datatable of individual Cases
+        $('#individualCaseTable').dataTable({
+            "order": [
+                [0, "desc"]
+            ],
+            "data": res["individualCases"],
+            "columns": [{
+                    "data": "id"
+                }, {
+                    "data": "dte"
+                }, {
+                    "data": "province"
+                }, {
+                    "data": "city"
+                }, {
+                    "data": "age"
+                }, {
+                    "data": "travel_history"
+                }, {
+                    "data": "confirmed_presumptive"
+                },
+                {
+                    "data": "source",
+                    "render": (data, type, row, meta) => {
+                        if (type === 'display')
+                            data = '<a href="' + data + '">Source</a>';
 
-		}
-
-
-
-		// $('#totalDeath')[0].innerHTML = deaths_total + " ";
-		// $('.death_total')[0].innerHTML = deaths_total;
-		
-		// draw 3 main graphs
-		lineGraph(res["cumulativeCases"], "#cumulativeCaseChart", true);
-		lineGraph(res["dailyCases"], "#dailyCaseChart", false);
-		barGraph(res["totalCaseProvince"], "#provinceCasesChart");
-
-		// update datatable of individual Cases
-		$('#individualCaseTable').dataTable({
-			"order": [[0, "desc"]],
-			"data": res["individualCases"],
-			"columns": [{
-					"data": "id"
-				}, {
-					"data": "dte"
-				}, {
-					"data": "province"
-				}, {
-					"data": "city"
-				}, {
-					"data": "age"
-				}, {
-					"data": "travel_history"
-				}, {
-					"data": "confirmed_presumptive"
-				},
-				{
-					"data": "source",
-					"render": (data, type, row, meta) => {
-						if (type === 'display')
-							data = '<a href="' + data + '">Source</a>';
-
-						return data;
-					}
-				}
-			]
-		});
-	}, err => {
-		console.log(err)
-	})
+                        return data;
+                    }
+                }
+            ]
+        });
+    }, err => {
+        console.log(err)
+    })
 
 });
 
 function goAdvanced() {
-	window.location.href = url + "advanced.html";
+    window.location.href = url + "advanced.html";
 }
